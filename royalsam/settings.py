da8f38d73e7780139c803dc774e5f452fc5cf9eb
sam2829 <>
2023-10-23 08:52:43 +0000

    Change allowed hosts address for heroku app

--- royalsam/settings.py
+++ royalsam/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-sam2829-project-4-royal-t5bmyslqh7.us2.codeanyapp.com', 'royal-sam.herokuapp.com']
+ALLOWED_HOSTS = ['8000-sam2829-project-4-royal-t5bmyslqh7.us2.codeanyapp.com', 'royal-sam-02fa0ed9cfe2.herokuapp.com']
 
 
 # Application definition
