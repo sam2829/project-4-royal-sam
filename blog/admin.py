9d564a63ef14d6ae4caafdf6bba65cf454e121c7
sam2829 <>
2023-10-24 07:52:45 +0000

    Create Post and Comment classes in the admin.py file

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,33 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'status', 'created_on', 'approved')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
+    actions = ['approve_posts']
+
+    # This method is called to approve a post before it can be displayed
+
+    def approve_posts(self, request, queryset):
+        queryset.update(approved=True)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    # This method is called to approve a comment before it can be displayed
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
