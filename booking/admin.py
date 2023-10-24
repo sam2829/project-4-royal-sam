7e69503c8b05ca6e298b36eec6f4c59ca606f711
sam2829 <>
2023-10-24 08:09:08 +0000

    Create BookingAdmin class in the admin.py file

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Booking
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Booking)
+class BookingAdmin(SummernoteModelAdmin):
+
+    list_display = ('user', 'email', 'date', 'time',
+                    'number_of_players', 'member', 'buggy')
+    search_fields = ['user', 'email', 'date', 'time']
+    list_filter = ('date', 'time')
+    summernote_fields = ('content')
