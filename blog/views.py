7e9c7e1f50523dfe74c868851d07edbe684caab1
sam2829 <>
2023-10-30 11:48:43 +0000

    Create view in views.py for the posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+# This class is to create a view to display posts on the reviews page
+
+class PostList(generic.ListView):
+    
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'reviews.html'
+    paginate_by = 5
