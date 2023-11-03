79c4d827608a9f13a997ec5c6ad35d67908daf93
sam2829 <>
2023-11-03 11:30:28 +0000

    Add view, urls.py and style for user being able to like and unlike a post when logged in

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='reviews'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='review_details')
+    path('<slug:slug>/', views.PostDetail.as_view(), name='review_details'),
+    path('like/<slug:slug>/', views.PostLike.as_view(), name='review_like'),
 ]
