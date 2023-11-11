4ed02a98e4c305892e7950b5d5cf358af5783b04
sam2829 <>
2023-11-11 11:35:59 +0000

    Add booking forms.py file and create booking form

---
+++ booking/forms.py
@@ -0,0 +1,14 @@
+from .models import Booking
+from django import forms
+
+# This class is for the booking form to book a tee time
+# and which fields I want to be shown
+
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('email', 'date', 'time', 'number_of_players', 'member',
+                 'buggy',)
+
+
