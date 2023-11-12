0dd38fed55e06e4f99a6fce23865a34b6f91336c
sam2829 <>
2023-11-12 16:32:48 +0000

    Add BookATime view and modify BookATee post method so only available times are displayed when booking_form_time is displayed

--- booking/forms.py
+++ booking/forms.py
@@ -15,6 +15,13 @@ class BookingFormDate(forms.ModelForm):
 
 class BookingFormTime(forms.ModelForm):
 
+    time = forms.ChoiceField(choices=[])
+
+    def __init__(self, *args, **kwargs):
+        available_times = kwargs.pop('available_times', [])
+        super().__init__(*args, **kwargs)
+        self.fields['time'].choices = [(time, time) for time in available_times]
+
     class Meta:
         model = Booking
         fields = ('time', 'number_of_players', 'member', 'buggy',)
--- booking/views.py
+++ booking/views.py
@@ -25,33 +25,48 @@ class BookATee(View):
     def post(self, request, *args, **kwargs):
 
         booking_form_date = BookingFormDate(data=request.POST)
+        print(request.POST)
 
         if booking_form_date.is_valid():
             booking_date = booking_form_date.save(commit=False)
             booking_date.user = request.user
-            booking_date.save()
+            selected_date = booking_form_date.cleaned_data['date']
+            selected_date_str = selected_date.isoformat()
+            request.session['date'] = selected_date_str
+           
             return redirect('book_a_time')
         else:
-            booking_form_date = BookingFormDate()
-
-        return render(
-            request,
-            "book_a_tee.html",
-            {
-                "booking_form_date": BookingFormDate(),
-            },
-        )
+            return render(
+                request,
+                "book_a_tee.html",
+                {
+                    "booking_form_date": BookingFormDate(),
+                },
+            )
     # this is so the user can view the booking time form
 
 class BookATime(View):
 
     # Booking time form appears, user must select date and email
     def get(self, request, *args, **kwargs):
+       
+        selected_date_str = request.session.get('date')
+
+        if selected_date_str:
+            selected_date = datetime.strptime(
+                selected_date_str, '%Y-%m-%d').date()
+            existing_times = Booking.objects.filter(
+                date=selected_date).values_list('time', flat=True)
+            # Replace YourModel and TIME_CHOICES with your actual model and choices
+            available_times = [
+                time[0] for time in AVAILABLE_TIMES if time[0] not in existing_times]
+        else:
+            available_times = [time[0] for time in AVAILABLE_TIMES]
 
         return render(
             request,
             "book_a_time.html",
             {
-                "booking_form_time": BookingFormTime(),
+                "booking_form_time": BookingFormTime(available_times=available_times),
             },
         )
