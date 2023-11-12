34621b513747c90fa2ef6ce169e12e0770a0948a
sam2829 <>
2023-11-12 15:26:28 +0000

    Add post method to BookATee class which redirects to book_a_time view when form valid

--- booking/urls.py
+++ booking/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.BookATee.as_view(), name='book_a_tee'),
-    path('booking_time/', views.BookATee.as_view(), name='book_a_time'),
+    path('booking_time/', views.BookATime.as_view(), name='book_a_time'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views import generic, View
 from django.http import HttpResponseRedirect
 from .models import *
@@ -9,6 +9,7 @@ from .forms import BookingFormDate, BookingFormTime
 
 class BookATee(View):
 
+    # Booking date form appears, user must select date and email
     def get(self, request, *args, **kwargs):
         
         
@@ -20,18 +21,37 @@ class BookATee(View):
             },
         )
 
+    # User has selected date and posts form
     def post(self, request, *args, **kwargs):
+
+        booking_form_date = BookingFormDate(data=request.POST)
+
+        if booking_form_date.is_valid():
+            booking_date = booking_form_date.save(commit=False)
+            booking_date.user = request.user
+            booking_date.save()
+            return redirect('book_a_time')
+        else:
+            booking_form_date = BookingFormDate()
+
+        return render(
+            request,
+            "book_a_tee.html",
+            {
+                "booking_form_date": BookingFormDate(),
+            },
+        )
+    # this is so the user can view the booking time form
+
+class BookATime(View):
+
+    # Booking time form appears, user must select date and email
+    def get(self, request, *args, **kwargs):
 
         return render(
             request,
             "book_a_time.html",
             {
                 "booking_form_time": BookingFormTime(),
-            }
+            },
         )
-
-
-    # this is so the user can post the booking a tee time form
-
-
-    
