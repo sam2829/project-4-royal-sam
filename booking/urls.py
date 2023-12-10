73115574450a17b88198015adb3e5a9e03273c9a
sam2829 <>
2023-12-10 14:42:26 +0000

    Add code so user can edit one of their bookings

--- booking/urls.py
+++ booking/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.BookATee.as_view(), name='book_a_tee'),
     path('booking_time/', views.BookATime.as_view(), name='book_a_time'),
     path('my_bookings/', views.MyBookings.as_view(), name='my_bookings'),
-    path('edit_booking/', views.EditBooking.as_view(), name='edit_tee_time'),
+    path('edit_booking_date/<item_id>/', views.EditBookingDate.as_view(), name='edit_tee_date'),
+    path('edit_booking_time/<item_id>/', views.EditBookingTime.as_view(), name='edit_tee_time'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -147,21 +147,145 @@ class MyBookings(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         # Filter to show bookings for the user ordered by date and time
-        bookings = Booking.objects.filter(user=self.request.user).order_by('date', 'time')
+        bookings = Booking.objects.filter(
+            user=self.request.user).order_by('date', 'time')
 
         return bookings
 
-# This class is for the user to edit a booking
+# This class is for the user to edit a booking date
 
-class EditBooking(View):
 
-    # Booking date form appears, user must select date and email
-    def get(self, request, *args, **kwargs):
+class EditBookingDate(View):
 
+    # Gets the the form to edit date and displays the original data in form
+    def get(self, request, item_id, *args, **kwargs):
+        booking = get_object_or_404(Booking, id=item_id)
+        booking_form_date = BookingFormDate(instance=booking)
+        return render(
+            request,
+            "edit_tee_date.html",
+            {
+                "booking_form_date": booking_form_date,
+            },
+        )
+
+    # User has selected date and posts form
+
+    def post(self, request, item_id, *args, **kwargs):
+
+        booking = get_object_or_404(Booking, id=item_id)
+        booking_form_date = BookingFormDate(request.POST, instance=booking)
+
+        if booking_form_date.is_valid():
+            booking_date = booking_form_date.save(commit=False)
+            booking_date.user = request.user
+            selected_date = booking_form_date.cleaned_data['date']
+            selected_date_str = selected_date.isoformat()
+            request.session['date'] = selected_date_str
+            booking_email = request.POST.get('email')
+            request.session['booking_email'] = booking_email
+
+            return redirect('edit_tee_time', item_id=booking.id)
+        else:
+            return render(
+                request,
+                "edit_tee_date.html",
+                {
+                    "booking_form_date": BookingFormDate(),
+                },
+            )
+
+# This class is for user to edit booking time
+
+
+class EditBookingTime(View):
+
+    # Booking time form appears, user must select date and email
+    def get(self, request, item_id, *args, **kwargs):
+
+        selected_date_str = request.session.get('date')
+        booking = get_object_or_404(Booking, id=item_id)
+        booking_form_time = BookingFormTime(instance=booking)
+
+        if selected_date_str:
+            selected_date = datetime.strptime(
+                selected_date_str, '%Y-%m-%d').date()
+            existing_times = Booking.objects.filter(
+                date=selected_date).values_list('time', flat=True)
+
+            # Filter out times that are in the past
+            current_time = timezone.now().time()
+            available_times = [
+                time[0] for time in AVAILABLE_TIMES
+                if time[0] not in existing_times and (
+                    selected_date != timezone.now().date() or
+                    datetime.combine(selected_date, datetime.strptime(time[0], '%H:%M').time(
+                    )) > datetime.combine(timezone.now().date(), current_time)
+                )
+            ]
+        else:
+            available_times = [
+                time[0] for time in AVAILABLE_TIMES if (
+                    selected_date != timezone.now().date() or
+                    datetime.combine(selected_date, time[0]) > datetime.combine(
+                        timezone.now().date(), current_time)
+                )
+            ]
+        messages.info(request, f'You have selected the date: {selected_date}')
         return render(
             request,
             "edit_tee_time.html",
             {
-                "booking_form_date": BookingFormDate(),
+                "booking_form_time": BookingFormTime(available_times=available_times),
             },
         )
+
+    # User has selected time and posts form
+
+    def post(self, request, item_id, *args, **kwargs):
+
+        selected_date_str = request.session.get('date')
+        booking = get_object_or_404(Booking, id=item_id)
+
+        if selected_date_str:
+            selected_date = datetime.strptime(
+                selected_date_str, '%Y-%m-%d').date()
+            existing_times = Booking.objects.filter(
+                date=selected_date).values_list('time', flat=True)
+            available_times = [time[0]
+                               for time in AVAILABLE_TIMES if time[0] not in existing_times]
+        else:
+            current_time = timezone.now().time()
+            available_times = [time[0] for time in AVAILABLE_TIMES if (
+                selected_date != timezone.now().date() or
+                datetime.combine(selected_date, time[0]) > datetime.combine(
+                    timezone.now().date(), current_time)
+            )]
+
+        booking_form_time = BookingFormTime(
+            data=request.POST, available_times=available_times, instance=booking)
+
+        if booking_form_time.is_valid():
+            booking_time = booking_form_time.save(commit=False)
+            booking_time.user = request.user
+            booking_email = request.session.get('booking_email')
+            booking_time.email = booking_email
+
+            if selected_date_str:
+                booking_time.date = selected_date
+
+            booking_time.save()
+
+            messages.success(request, f' You have successfully booked your tee '
+                             f'time for: {booking_time.user}, {selected_date} '
+                             f'at {booking_time.time} for {booking_time.number_of_players}.')
+            return redirect('my_bookings')
+
+        else:
+            return render(
+                request,
+                "edit_tee_time.html",
+                {
+                    "booking_form_time": booking_form_time,
+                },
+            )
