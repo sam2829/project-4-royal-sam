a257631922facfad46bc33bec088004f8b410938
sam2829 <>
2024-01-09 15:57:25 +0000

    Remove error 500 test code.

--- gallery/views.py
+++ gallery/views.py
@@ -8,10 +8,8 @@ from .models import GalleryImage
 
 class Gallery(TemplateView):
 
-    #def get(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
 
-        #images = GalleryImage.objects.all()
-        #return render(request, 'gallery.html', {'images': images})
-    def my_error_view(request):
-        # Simulate an error by raising an exception
-    raise Exception("Intentional error for testing 500 page")
+        images = GalleryImage.objects.all()
+        return render(request, 'gallery.html', {'images': images})
+      
